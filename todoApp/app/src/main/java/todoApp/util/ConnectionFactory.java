4f4b0f4193263d350c7b0ae42514621ae19ed23b
Lupesto <lucasthe2@gmail.com>
2022-06-16 11:54:52 -0300

    projeto

--- todoApp/app/src/main/java/todoApp/util/ConnectionFactory.java
+++ todoApp/app/src/main/java/todoApp/util/ConnectionFactory.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package todoApp.util;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 /**
  *
@@ -11,8 +13,32 @@ package todoApp.util;
 public class ConnectionFactory {
     
     public static final String DRIVER = "com.mysql.jdbc.Driver";//precisa de driver de conexão. Diz ao programa qual banco de dados
-
     public static final String URL = "jdbc:mysql://localhost/3306/todoapp";//Caminho de conexão até o banco de dados. Porta aparece lá no xamp.
     public static final String USER = "root" ;//mySQL, por padrão usuário root e sem senha
     public static final String PASS = "";
+    
+    public static Connection getConnection() {
+    	try{
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL,USER,PASS); //exige 3 parametros para conexão
+	} catch(Exception ex){
+            throw new RuntimeException("Erro conexão banco de dados", ex);
+	}
+    }
+    
+    public static void closeConnection(Connection connection){
+	try{
+            if(connection != null) //identificar se conexão existe.
+            connection.close();
+        }
+	catch (Exception ex){
+		throw new RuntimeException("Erro ao fechar conexão com banco de dados");
+	}
+
+        
+    }
+
+    
+    
+    
 }
